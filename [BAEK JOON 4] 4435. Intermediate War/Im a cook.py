e162aaf03ad514e48f50c6483f4c69abd19bcded
JH-Ko <72744580+JH-Ko76@users.noreply.github.com>
2024-04-29 22:28:22 +0900

    Delete [BAEK JOON 4] 4435. Intermediate War directory

--- [BAEK JOON 4] 4435. Intermediate War/Im a cook.py
+++
@@ -1,8 +0,0 @@
-score = [];
-number = 0;
-for i in range (5) :
-    b = list(map(int, input().split()))
-    score.append(sum(b));
-    number = score.index(max(score));
-
-print(number+1,max(score));
