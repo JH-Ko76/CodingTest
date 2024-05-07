aa48b9a503faa3e7f75528ebfa515f822e26a36b
JH-Ko <72744580+JH-Ko76@users.noreply.github.com>
2024-05-08 01:13:53 +0900

    Add files via upload

---
+++ [BAEK JOON] 10~19/[BAEK JOON 12] 2851.Super Mario/super_mario.py
@@ -0,0 +1,14 @@
+mushroom = []
+score = 0
+for i in range(10):
+    mushroom.append(int(input()))
+
+for j in mushroom:
+    score += j
+    if score >= 100:
+        if (score-100) <= 100 - (score-j):
+            break
+        else:
+            score-=j
+            break
+print(score)
